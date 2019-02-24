f59301304d35feefdeeca3827b1cd5c6cb36ce2c
Tankcaster <tankcaster@gmail.com>
2019-02-24 04:24:19 -0600

    crops before saving now

--- panorama.js
+++ panorama.js
@@ -1,6 +1,6 @@
 const iohook = require('iohook');
 const screenshot = require('screenshot-desktop');
-const fs = require('fs');
+const jimp = require('jimp');
 const config = require('./config.json');
 const ffi = require('ffi'),
 user32 = ffi.Library('user32', {
@@ -8,31 +8,46 @@ user32 = ffi.Library('user32', {
 });;
 
 
-
+//listen for hotkey
 iohook.on('keydown', event => {
     if(event.rawcode == 80 && event.altKey) {
-        console.log('hot key triggered');
 
+        console.log('hot key triggered');
         
         for(let i = 0; i < 16; i++) {
             
+            //small delay for clear screenshots
             setTimeout(() => {
-                let filename = 'screenshot' + i + '.jpg';
-                screenshot()
-                    .then((img) => {
-                        console.log(`writing ${config.screenshotFolder + filename}`);
-                        fs.writeFile(config.screenshotFolder + filename, img, (err) => 
-                            err ? console.log : null
-                        );
-                    })
-                    .catch(err => console.error);
 
+                //take screenshot
+                let filename = 'screenshot' + i + '.jpg';
+                handleScreenshot(config.screenshotFolder + filename);
+
+                //move mouse
                 user32.mouse_event(1, 500 ,0 ,0 ,0);
 
-            }, 1000 * i);
-        }
-        
+            }, 500 * i);
+        }   
     }
 });
 
+//takes screenshot, crops, and saves it to 'outputFile'
+handleScreenshot = outputFile => {
+
+    screenshot() //take screenshot
+    .then((imgData) => {
+
+        //crop and save
+        jimp.read(imgData)
+        .then(image => {
+            image
+                .crop(300, 130, 1600, 770)
+                .write(outputFile);
+        });
+    })
+    .catch(err => console.error);
+}
+
+//start listening for hotkey
 iohook.start();
+console.log("\x1b[32m%s\x1b[0m", 'Ready and listening for hotkey...')
